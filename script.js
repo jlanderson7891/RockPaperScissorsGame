38014babb76db4d0a5cd7e699b36dd5227661148
Jacob Anderson <jlanderson7891@gmail.com>
2022-12-14 22:37:05 -0600

    created computer random choice

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-//array of selections
-const selArray = ["Rock", "Paper", "Scissors"];
+//computer input
+function getComputerChoice() {
+	const selArray = ["Rock", "Paper", "Scissors"];
+
+	let randomChoice = selArray[Math.floor(Math.random() * selArray.length)];
+
+	return randomChoice;
+
+	// let randomNum = Math.floor(Math.random() * 3);
+
+	// let randomChoice = selArray.fromIndex(randomNum);
+
+	// return randomChoice;
+}
+
+console.log(getComputerChoice());
 
 //user input
-const userChoice = prompt("Make your selection: Rock, Paper, Scissors");
+// const userChoice = prompt("Make your selection: Rock, Paper, Scissors!");
+
+//computer input
